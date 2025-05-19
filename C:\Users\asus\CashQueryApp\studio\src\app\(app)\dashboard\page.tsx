9dad0d076fa0095633a9e95bd918b36831cf6cc0
Mish <dhannawikumari12@gmail.com>
2025-05-19 05:41:20 +0000

    see if i have deleted any important file

--- C:\Users\asus\CashQueryApp\studio\src\app\(app)\dashboard\page.tsx
+++
@@ -1,5 +0,0 @@
-// This file is no longer used as its content has been moved to src/app/(app)/page.tsx
-// and this route segment (/dashboard) is effectively removed.
-export default function DashboardPageMoved() {
-  return null;
-}
